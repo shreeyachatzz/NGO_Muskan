1c69f6c6f53c94facc77726072eabc22f11a2afa
SHREEYA CHATTERJI <schatterji_be21@thapar.edu>
2022-12-30 13:28:06 +0530

    added counter and additional support and donate blocks

--- start.js
+++ start.js
@@ -21,3 +21,19 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
 }
+
+let valueDisplays = document.querySelectorAll(".count");
+let interval = 5000;
+
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 500;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval/endValue);
+  let counter = setInterval(function(){
+    startValue +=1;
+    valueDisplay.textContent = startValue;
+    if(startValue == endValue){
+      clearInterval(counter);
+    }
+  }, duration);
+});
